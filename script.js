e91b21c7efe097cab7b57259e8d37191e4cc5537
taipp <clipsrealbond@gmail.com>
2025-01-29 12:50:01 +0200

    Added changing of icons

--- script.js
+++ script.js
@@ -12,12 +12,66 @@ async function checkWeather(city) {
     return data;
 }
 
+function updateWeatherIcon(data) {
+    const iconElem = document.getElementById("weather-icon");
+    const weatherCode = data.weather[0].main;
+
+    if (weatherCode.toLowerCase() === "clear sky") {
+        iconElem.src = "imgOpenWeather/clear-sky.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "few clouds") {
+        iconElem.src = "imgOpenWeather/few-clouds.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "scattered clouds" || weatherCode.toLowerCase() === "clouds") {
+        iconElem.src = "imgOpenWeather/scattered-clouds.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "broken clouds") {
+        iconElem.src = "imgOpenWeather/broken-clouds.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "shower rain") {
+        iconElem.src = "imgOpenWeather/shower-rain.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "rain") {
+        iconElem.src = "imgOpenWeather/rain.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "thunderstorm") {
+        iconElem.src = "imgOpenWeather/thunderstorm.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "snow") {
+        iconElem.src = "imgOpenWeather/snow.png";
+    }
+
+    else if (weatherCode.toLowerCase() === "mist") {
+        iconElem.src = "imgOpenWeather/mist.png";
+    }
+
+    else {
+        iconElem.src = "imgOpenWeather/clear-sky.png";
+    }
+
+}
+
 function updateWeatherData(data){
+    updateWeatherIcon(data);
+
     document.getElementById("temperature-info").innerHTML = data.main.temp.toFixed(1) + "\u00B0c";
     document.getElementById("city-name").innerHTML = data.name;
     document.getElementById("humidity-info").innerHTML = data.main.humidity + "%";
     document.getElementById("wind-info").innerHTML = data.wind.speed + "km/h";
 
+    console.log(data.weather[0].main);
+
+}
+
+function saveCity(city) {
+    localStorage.setItem("city", city);
 }
 
 function handleCityInput() {
@@ -29,6 +83,14 @@ function handleCityInput() {
     checkWeather(city)
         .then(data => updateWeatherData(data))
         .catch(error => console.log(`Error fetching data: `, error))
+}
+
+function saveData(){
+    localStorage.setItem("data", appCard.innerHTML);
+}
+
+function showTask(){
+    appCard.innerHTML = localStorage.getItem("data");
 }
 
 document.getElementById("search-button").addEventListener('click', handleCityInput);
